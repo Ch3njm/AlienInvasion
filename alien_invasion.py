102c6608c25902160f517959b44805cd71e8514b
Chen's PC <chenjm_3000@outlook.com>
2017-09-30 22:31:03 +0800

    fix bug of quit and change the color of window

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,21 +1,29 @@
 import sys
 
 import pygame
+
 def run_game():
     pygame.init()
     screen = pygame.display.set_mode((1200,800))
     pygame.display.set_caption("Alien Invasion")
-
-
+    bg_color = (230, 230, 230)
+    flag = 1
+    
     #start the main loop of the game
-    while True:
+    while flag:
+
+        
+        screen.fill(bg_color)
+        
+        #display recent screen
+        pygame.display.flip()
 
         #monitor the mouse and keyborad event
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    sys.exit()
-        #display recent screen
-        pygame.display.flip()
+                    pygame.quit()
+                    flag = 0
 
-
+                    
+        
 run_game()
